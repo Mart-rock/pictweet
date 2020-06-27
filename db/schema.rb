3145dd5c97b30a8e7a2400389502455d1b42d1bf
Mart-rock <itsmyrockandrollmart34@gmail.com>
2020-06-27 20:10:29 +0900

    hidouki

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 2020_06_24_081423) do
-=======
 ActiveRecord::Schema.define(version: 2020_06_24_054350) do
->>>>>>> Stashed changes
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
